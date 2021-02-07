6c979c5e230ca98645b425285c7da2b2e7020b39
DESKTOP-D95G5CT\Shorobura <shorobyramax@gmail.com>
2021-02-07 19:43:49 +0200

    Our changes

--- задачка/Program.cs
+++ задачка/Program.cs
@@ -6,32 +6,44 @@ namespace задачка
     {
         static void Main(string[] args)
         {
-                        
-            int hStart = int.Parse(Console.ReadLine());
-            int hEnd = int.Parse(Console.ReadLine());
-            int vStart = int.Parse(Console.ReadLine());
-            int vEnd = int.Parse(Console.ReadLine());
+
+            int hStart = 1;// int.Parse(Console.ReadLine());
+            int hEnd = 10;// int.Parse(Console.ReadLine());
+            int vStart = 1;// int.Parse(Console.ReadLine());
+            int vEnd = 10;// int.Parse(Console.ReadLine());
             int x = hStart;
             int y = vStart;
             int max = hEnd * vEnd;
             int length = 0;
             int a = hEnd;
             int b = vEnd;
-            int d = hEnd - hStart;
+            int d = hEnd - hStart + 1;
             int maxCount = max;
-            
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(max);
+            //Console.WriteLine(length);
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(d);
+            //Console.WriteLine(maxCount);
+
             while (maxCount >= 1)
             {
                 length++;
                 maxCount = maxCount / 10;
             }
+            //Console.WriteLine(length);
+            //Console.WriteLine(maxCount);
 
-            for (int i = 0; i <= length; i++)
+            for (int i = 0; i < length; i++)
             {
                 Console.Write(" ");
+                
             }
 
-            Console.Write(" |");
+                Console.Write(" |");
 
             x = hStart;
             while (x <= hEnd)
@@ -41,11 +53,11 @@ namespace задачка
                 while (longLine >= 1)
                 {
                     countLine++;
-                    longLine = longLine / ( d + 1);
+                    longLine = longLine / 10;
                 }
 
                 for (int i = 0; i <= length - countLine; i++)
-                { 
+                {
                     Console.Write(" ");
                 }
                 Console.Write(x);
@@ -53,69 +65,63 @@ namespace задачка
             }
             Console.WriteLine();
 
-            for(int i = 0; i <= length; i++)
-            {
-                Console.Write(" ");
-            }
-            Console.Write("-+");
-
-            for(int Line = hStart; Line <= hEnd; Line++)
+            for (int i = 0; i <= length; i++)
             {
                 Console.Write("-");
-                int LineLong = 0;
-                while(LineLong <= length)
-                {
-                    Console.Write("-");
-                    LineLong++;
-                }
             }
+            Console.Write("+");
+
+            for (int Line = 1; Line <= d * (length + 1); Line++)
+            {
+                Console.Write("-");
+            }
+
             Console.WriteLine();
             x = hStart;
             while (x <= vEnd)
             {
-                    
                 while (vStart <= vEnd)
                 {
                     int LongLine = vStart;
                     int countLongLine = 0;
-                    while(LongLine >= 1)
+                    while (LongLine >= 1)
                     {
                         countLongLine++;
-                        LongLine = LongLine / (d + 1);
+                        LongLine = LongLine / 10;
                     }
-                    
-                    for(int i = 0; i <= length - countLongLine; i++)
+
+                    for (int i = 0; i <= length - countLongLine; i++)
                     {
-                        Console.Write("  ");
+                        Console.Write(" ");
                     }
                     Console.Write(vStart + "|");
-                    
+
                     x = hStart;
-                    while(x <= hEnd)
+                    while (x <= hEnd)
                     {
                         int product = vStart * x;
                         int LineLong = product;
                         int countLong = 0;
-                        while(LongLine >= 1)
+                        while (LongLine >= 1)
                         {
                             countLong++;
                             LineLong = LineLong / (d + 1);
                         }
 
-                        for(int i = 0; i <= length - countLong; i++)
+                        for (int i = 0; i <= length - countLong; i++)
                         {
                             Console.Write(" ");
                         }
-                        
+
                         Console.Write(product);
                         x++;
                     }
                     Console.WriteLine();
                     vStart++;
                 }
-                
+
             }
-             Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
